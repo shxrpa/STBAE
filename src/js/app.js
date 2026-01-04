bb8a703b8c86e719865b205ec17da2b9b007e4d2
Erik Fisher <erik@shxrpa.com>
2026-01-04 13:42:46 -0600

    SHX-173: Setup basic event handling structure in src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
 // STBAE - Should This Be an Email? - Main Application Logic
 
-// Application logic will be added in subsequent tasks
+/**
+ * Initialize the application
+ */
+function initializeApp() {
+    // Application initialization logic will be added in subsequent tasks
+}
 
+// Initialize app when DOM is ready
+document.addEventListener('DOMContentLoaded', initializeApp);
